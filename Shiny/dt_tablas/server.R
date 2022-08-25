4cb4730d431bb84400cf9f654f0f8bf20b91f53a
Marce Melgar <marcelamelgar@office.ufm.edu>
2022-08-24 18:06:12 -0600

    multiple select y single select en tablas

--- Shiny/dt_tablas/server.R
+++ Shiny/dt_tablas/server.R
@@ -1,49 +1,97 @@
 library(shiny)
-library(DT)
 library(dplyr)
 library(ggplot2)
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
   
-  output$tabla_1 <- renderDataTable({
-    mtcars %>% datatable(rownames = FALSE, 
-                         selection = 'single',
-                         filter = 'top',
-                         options = list(scrollX = TRUE)
-    )
-  })
-  output$output_1 <- renderText({
-    input$tabla_1_rows_selected
-  })
-  
-  output$tabla_2 <- renderDataTable({
-    mtcars %>% datatable(rownames = FALSE,
-                         filter = 'top',
-                         options = list(scrollX = TRUE)
-    )
-  })
-  output$output_2 <- renderText({
-    input$tabla_2_rows_selected
+  output$tabla_1 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(rownames = FALSE,
+                             filter = 'top',
+                             extensions = 'Buttons',
+                             options = list(
+                               pageLength = 5,
+                               lengthMenu = c(5, 10, 15),
+                               dom = 'Bfrtip',
+                               buttons = c('csv')
+                               
+                             ) )
   })
   
-  output$tabla_3 <- renderDataTable({
-    diamonds %>%
-      mutate(vol = x*y*z,
+  output$tabla_2 <- DT::renderDataTable({
+    diamonds %>% 
+      mutate(vol= x*y*z,
              vol_promedio = mean(vol),
-             volp = vol/vol_promedio-1
-      )%>%
-      datatable(filter = 'top',
-                selection = list(
-                  mode = 'single',
-                  target = 'column'),
-                options = list(
-                  scrollX = TRUE
-                ))%>%
-      formatCurrency(columns = 'price',currency = '$')
+             volp=vol/vol_promedio-1) %>% 
+      DT::datatable(filter = 'top') %>% 
+      formatCurrency(columns = 'price',
+                     currency = '$') %>% 
+      formatPercentage('volp',digits = 2)
   })
   
-  output$output_3 <- renderText({
-    input$tabla_3_columns_selected
+  
+  output$tabla_3 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = 'single',
+                             options = list(scrollX = TRUE))
   })
+  
+  
+  output$output_1 <- renderPrint({
+    input$tabla_3_rows_selected
+  })
+  
+  output$tabla_4 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(options = list(scrollX = TRUE))
+  })
+  
+  
+  output$output_2 <- renderPrint({
+    input$tabla_4_rows_selected
+  })
+  
+  output$tabla_5 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='single',
+                                              target='column'),
+                             options = list(scrollX = TRUE))
+  })
+  
+  
+  output$output_3 <- renderPrint({
+    input$tabla_5_columns_selected
+  })
+  
+  output$tabla_6 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='multiple',
+                                              target='column'),
+                             options = list(scrollX = TRUE))
+  })
+  
+  
+  output$output_4 <- renderPrint({
+    input$tabla_6_columns_selected
+  })
+  
+  
+  output$tabla_7 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='single',
+                                              target='cell'),
+                             options = list(scrollX = TRUE))
+  })
+  
+  
+  output$output_5 <- renderPrint({
+    input$tabla_7_cells_selected
+  })
+  
+  output$tabla_8 <- DT::renderDataTable({
+    mtcars %>% DT::datatable(selection = list(mode='multiple',
+                                              target='cell'),
+                             options = list(scrollX = TRUE))
+  })
+  
+  
+  output$output_6 <- renderPrint({
+    input$tabla_8_cells_selected
+  })
+  
   
 })
--- Shiny/dt_tablas/ui.R
+++ Shiny/dt_tablas/ui.R
@@ -1,34 +1,54 @@
-
 library(shiny)
 library(DT)
 
+
 shinyUI(fluidPage(
-
-    titlePanel("Tablas en Shiny"),
-
-    fluidRow(column(6,
-                    h1('Single Select Row'),
-                    dataTableOutput('tabla_1'),
-                    verbatimTextOutput('output_1')
-                    ),
-             
-             column(6,
-                    h1('Multiple Select Row'),
-                    dataTableOutput('tabla_2'),
-                    verbatimTextOutput('output_2')
-                    )
+  
+  titlePanel("Todo sobre tablas en shiny"),
+  
+  tabsetPanel(
+    tabPanel("Tablas en DT",
+             h1('Vista Basica'),
+             fluidRow(
+               column(12,DT::dataTableOutput("tabla_1"))
              ),
-    
-    fluidRow(column(6,
-                    h1('Single Select Column'),
-                    dataTableOutput('tabla_3'),
-                    verbatimTextOutput('output_3')
+             h1("Agregar filtros"),
+             fluidRow( column(12, DT::dataTableOutput("tabla_2")))
     ),
-    
-    column(6,
-           h1('Multiple Select Column'),
-           dataTableOutput('tabla_4'),
-           verbatimTextOutput('output_4')
+    tabPanel("click en tablas",
+             fluidRow(column(6,
+                             h2('Single Select row'),
+                             DT::dataTableOutput('tabla_3'),
+                             verbatimTextOutput('output_1')),
+                      column(6,
+                             h2('Multiple Select rows'),
+                             DT::dataTableOutput('tabla_4'),
+                             verbatimTextOutput('output_2')
+                      )
+             ),
+             fluidRow(column(6,
+                             h2('Single Select cols'),
+                             DT::dataTableOutput('tabla_5'),
+                             verbatimTextOutput('output_3')
+             ),
+             column(6,
+                    h2('Multiple Select cols'),
+                    DT::dataTableOutput('tabla_6'),
+                    verbatimTextOutput('output_4')
+             )
+             ),
+             fluidRow(column(6,
+                             h2('Single Select Celda'),
+                             DT::dataTableOutput('tabla_7'),
+                             verbatimTextOutput('output_5')
+             ),
+             column(6,
+                    h2('Multiple Select Celda'),
+                    DT::dataTableOutput('tabla_8'),
+                    verbatimTextOutput('output_6')
+             )
+             )
     )
-    )
+  )
+  
 ))
