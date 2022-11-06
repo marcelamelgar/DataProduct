0a0a9ca0ab9954b75d6073e0ea88fd60ad312185
Marce Melgar <marcelamelgar@office.ufm.edu>
2022-11-06 00:34:53 -0600

    parte de eventos terminada

--- ShinyProject/ShinyOlimpics/server.R
+++ ShinyProject/ShinyOlimpics/server.R
@@ -1,7 +1,27 @@
 library(shiny)
+library(crosstalk)
+library(DT)
+library(dplyr)
+library(tidyverse)
 
-shinyServer(function(input, output) {
-
-    
+shinyServer(function(input, output, session) {
+  
+  Events <- athlete_events %>%
+    distinct(City, Year, Season) %>%
+    arrange(Year)
+  
+  output$tablaEventos <- renderDataTable({
+    dt <- Events %>%
+      filter(Year >= input$ChooseYear[1] & Year <= input$ChooseYear[2]) %>%
+      filter(City %in% input$selectHost) %>%
+      filter(Season %in% input$chkboxSeason)
+    dt
+  })
+  
+  observeEvent(input$clean,{
+    updatePickerInput(session, 'selectHost', choices = unique(sort(athlete_events$City)),options = list(`actions-box` = TRUE))
+    updateSliderInput(session, 'ChooseYear', value = c(min(athlete_events$Year), max(athlete_events$Year)))
+    updateCheckboxGroupInput(session, 'chkboxSeason', choices = unique(athlete_events$Season), selected=NULL, inline = TRUE)
+  })
 
 })
--- ShinyProject/ShinyOlimpics/ui.R
+++ ShinyProject/ShinyOlimpics/ui.R
@@ -3,6 +3,7 @@ library(shiny)
 library(shinythemes)
 library(markdown)
 library(lubridate)
+library(shinyWidgets)
 
 shinyUI(fluidPage(theme = shinytheme("sandstone"),
 
@@ -10,10 +11,27 @@ shinyUI(fluidPage(theme = shinytheme("sandstone"),
                tabPanel("Eventos",
                         sidebarLayout(
                           sidebarPanel(
-                            
+                            h2('Eventos Olimpicos'),
+                            h4('Complete los 3 filtros para ver su informacion'),
+                            br(),
+                            sliderInput('ChooseYear', 'Seleccione Rango de Años:',
+                                        value = c(min(athlete_events$Year), max(athlete_events$Year)),sep = "", 
+                                        min = min(athlete_events$Year), max = max(athlete_events$Year),
+                                        step = 4),
+                            br(),
+                            pickerInput('selectHost', 'Seleccione Ciudad Host:',
+                                        choices = unique(sort(athlete_events$City)),
+                                        options = list(`actions-box` = TRUE),
+                                        multiple = T),
+                            br(),
+                            checkboxGroupInput('chkboxSeason', 'Seleccione Temporada:',
+                                               choices = unique(athlete_events$Season),
+                                               selected = NULL, inline = TRUE),
+                            br(),
+                            actionButton("clean","Limpiar")
                           ),
                           mainPanel(
-                            plotOutput("plot")
+                            dataTableOutput("tablaEventos")
                           )
                         )
                ),
